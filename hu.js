261d49414479b043b16789d2e7cf9e89fe06f027
gokhangokhan <64825842+gokhangokhan@users.noreply.github.com>
2020-05-07 07:16:33 +0300

    Update hu.js

--- hu.js
+++ hu.js
@@ -1,1 +1,9 @@
 $('.collapse').collapse()
+$("tr td").each(function () {
+           if ($(this).attr("aria-selected") === "true") {
+                $(this).parent().addClass('active')
+            } else {
+                $(this).parent().removeClass('active')
+
+            }
+        });
