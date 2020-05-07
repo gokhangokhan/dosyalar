b2c964a2696c455b0bc2642183678dd1fef3a7fc
gokhangokhan <64825842+gokhangokhan@users.noreply.github.com>
2020-05-07 07:27:33 +0300

    Update hu.js

--- hu.js
+++ hu.js
@@ -1,8 +1,6 @@
- $("tr td").each(function () {
-           if ($(this).attr("aria-selected") === "true") {
-                $(this).parent().addClass('active')
-            } else {
-                $(this).parent().removeClass('active')
-
-            }
+$(function () {
+  $('table tbody tr[tabindex]').each(function (i, e) {
+    var tabindex = e.getAttribute('tabindex');
+    console.log('tabindex=' + tabindex + ' Row text: ' + e.textContent);
+  });
 });
