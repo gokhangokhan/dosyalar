96eecc33686a5dc67acad0d9c2f268e965957a13
gokhangokhan <64825842+gokhangokhan@users.noreply.github.com>
2020-05-07 07:29:29 +0300

    Update hu.js

--- hu.js
+++ hu.js
@@ -1,6 +1,6 @@
 $(function () {
-  $('table tbody tr[tabindex]').each(function (i, e) {
-    var tabindex = e.getAttribute('tabindex');
+  $(table tbody tr ).each(function (i, e) {
+    var tabindex = e.getAttribute();
     console.log('tabindex=' + tabindex + ' Row text: ' + e.textContent);
   });
 });
